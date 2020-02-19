0a2ced6678e65c2623cc764368bd43804ef18c65
mc17 <marco.combosch@uni-ulm.de>
2020-02-19 21:00:18 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,8 @@
 import "@babel/polyfill";
-import UserController from "./controllers/UserController";
+import Channel from "./Channel";
 
 (async () => {
 
-    let response = await UserController.get({id: 2});
-    
-    if(response.was_success()) {
-        console.log("Success: ");
-        console.log(response.get_msg());
-    } else {
-        console.log("Error: " + response.get_msg());
-    }
-
-    response = await UserController.login("max.mustermann@mail.de", "123");
-    if(response.was_success()) {
-        console.log("Success: ");
-        console.log(response.get_msg());
-    } else {
-        console.log("Error: " + response.get_msg());
-    }
+    Channel.initialize("http://localhost:8080/");
 
 })();
